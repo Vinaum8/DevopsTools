1f8a5f3bb1845e69c56ac5cb15140c220cdf0a38
Vinícius Fernandes <vinaumpt@gmail.com>
2022-10-14 20:47:45 -0300

    Add files via upload

---
+++ InstallTools.ps1
@@ -0,0 +1,15 @@
+## Chocolatey Install
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+# Install Packages
+choco install docker-desktop
+
+choco install powershell-core
+
+choco install vscode
+
+choco install git
+
+choco install discord
+
+choco install 7zip
